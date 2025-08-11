cac4014a25a2844f8cd1b0a4827e09656c6b6efe
Prayson W. Daniel <prdani@norlys.dk>
2025-08-11 13:44:28 +0200

    working

--- src/ello/app.py
+++ src/ello/app.py
@@ -2,20 +2,35 @@ import chainlit as cl
 from ello.client import chat 
 
 
+
+
+@cl.on_chat_start
+def start_chat():
+    cl.user_session.set(
+        "message_history",
+        [{
+                "content": "You are Ello, you always reply in Emojis. Do not use words. Just emojis to communicate your message",
+                "role": "system"
+        }],
+    )
+
 @cl.on_message
 async def on_message(message: cl.Message):
-    response = await chat(
-        messages=[
-            {
-                "content": "You are a helpful bot, you always reply in Emojis",
-                "role": "system"
-            },
-            {
-                "content": message.content,
-                "role": "user"
-            }
-        ],
-        stream=False,
+    message_history = cl.user_session.get("message_history", [])
+    message_history.append({"role": "user", "content": message.content})
+
+
+    msg = cl.Message(content="")
+
+    stream = await chat(
+        messages=message_history,
+        stream=True,
     )
-    await cl.Message(content=response.choices[0].message.content).send()
+    
+    async for chunk in stream:
+        if token := chunk.choices[0].delta.content or "":
+            await msg.stream_token(token)
+    
+    message_history.append({"role": "assistant", "content": msg.content})
+    await msg.update()
 
